96b78551cc25809f6665779eb8e59ed9496f3bbb
Timzz <94833888+Timzz04@users.noreply.github.com>
2023-06-10 18:13:57 +0200

    Initial release
    -todo : add register page, fix the home page and maybe add encryption / security idk yet

---
+++ 5T_PROG_PROJECT_BANKAPP/5T_PROG_PROJECT_BANKAPP/AanmeldForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace _5T_PROG_PROJECT_BANKAPP
+{
+    partial class AanmeldForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TxtUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnInloggen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Aanmelden";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(353, 91);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TxtUsername
+            // 
+            this.TxtUsername.Location = new System.Drawing.Point(122, 208);
+            this.TxtUsername.Multiline = true;
+            this.TxtUsername.Name = "TxtUsername";
+            this.TxtUsername.Size = new System.Drawing.Size(122, 26);
+            this.TxtUsername.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Gebruikersnaam";
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Location = new System.Drawing.Point(122, 257);
+            this.TxtPassword.Multiline = true;
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.PasswordChar = '*';
+            this.TxtPassword.Size = new System.Drawing.Size(122, 26);
+            this.TxtPassword.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Passwoord";
+            // 
+            // BtnInloggen
+            // 
+            this.BtnInloggen.Location = new System.Drawing.Point(122, 332);
+            this.BtnInloggen.Name = "BtnInloggen";
+            this.BtnInloggen.Size = new System.Drawing.Size(120, 34);
+            this.BtnInloggen.TabIndex = 7;
+            this.BtnInloggen.Text = "Inloggen";
+            this.BtnInloggen.UseVisualStyleBackColor = true;
+            this.BtnInloggen.Click += new System.EventHandler(this.BtnInloggen_Click);
+            // 
+            // AanmeldForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 403);
+            this.Controls.Add(this.BtnInloggen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtUsername);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "AanmeldForm";
+            this.Text = "Aanmelden";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox TxtUsername;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnInloggen;
+    }
+}
+
---
+++ 5T_PROG_PROJECT_BANKAPP/5T_PROG_PROJECT_BANKAPP/MainScreen.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5T_PROG_PROJECT_BANKAPP
+{
+    public partial class MainScreen : Form
+    {
+
+        //Ik importeer dit voor het formulier rond te maken.
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+        //Declaraties
+        string filepath = "users.txt";
+        private string username;
+        private double balance;
+
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                username = value;
+                LblUsername.Text = username;
+            }
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+            set
+            {
+                balance = value;
+                LblBalance.Text = balance.ToString();
+            }
+        }
+
+
+
+
+
+
+
+        public MainScreen()
+        {
+            InitializeComponent();
+            //Hier zeg ik dat wanner de form laad het ronde edges moet heben
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 36, 36));
+   
+           
+        }
+
+
+
+
+
+
+
+
+        private void BtnDebugSettings_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Btnexit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void BtnMinimize_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void MainScreen_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
